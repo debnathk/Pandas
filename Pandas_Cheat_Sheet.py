51c7a92637599c12f493406bdfb2f96ad33f8fdc
Kusal Debnath <40787900+debnathk@users.noreply.github.com>
2020-09-08 20:34:21 +0530

    Pandas_Cheat_Sheet.py
    Some basic methods and their application in pandas.

--- Pandas_Cheat_Sheet.py
+++ Pandas_Cheat_Sheet.py
@@ -2,6 +2,10 @@
 #Importing Pandas to use the methods.
 import pandas as pd
 
+#---------------------------------------------------------------------------------------------------
+
+#Creating Data files - DataFrames & Series
+
 #To create DataFrames.
 first_df = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 print(first_df)
@@ -15,7 +19,6 @@ third_df = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'],
 print(third_df)
 
 
-
 #To create Series.
 first_se = pd.Series([1, 2, 3, 4, 5])
 print(first_se)
@@ -23,3 +26,12 @@ print(first_se)
 #To create Series using index.
 second_se = pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
 print(second_se)
+
+
+#Reading Data files - Comma Separated Values(CSV)
+first_df.to_csv("first_df.csv")
+print(first_df.shape)
+print("\n")
+print(first_df.head())
+
+#--------------------------------------------------------------------------------------------------------
