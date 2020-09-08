18bb488adcc96e270d292ecfe3f34bd26d07a796
Kusal Debnath <40787900+debnathk@users.noreply.github.com>
2020-09-08 19:42:29 +0530

    Update Pandas_Cheat_Sheet.py

--- Pandas_Cheat_Sheet.py
+++ Pandas_Cheat_Sheet.py
@@ -3,16 +3,23 @@
 import pandas as pd
 
 #To create DataFrames.
-pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
-pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+first_df = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
+print(first_df)
+second_df = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+print(second_df)
 
 #To create DataFrames using index.
-pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 
+third_df = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 
               'Sue': ['Pretty good.', 'Bland.']},
              index=['Product A', 'Product B'])
-             
+print(third_df)
+
+
+
 #To create Series.
-pd.Series([1, 2, 3, 4, 5])
+first_se = pd.Series([1, 2, 3, 4, 5])
+print(first_se)
 
 #To create Series using index.
-pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
+second_se = pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
+print(second_se)
